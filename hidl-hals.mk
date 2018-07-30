1cdd6addf805bb9a72da10668148bd2cb2d31cdc
barath-k <barath.kirubakaran@gmail.com>
2018-07-30 21:07:39 +0530

    gps: Update HAL to LA.UM.6.6.r1-06700-89xx.0.
    * tag LA.UM.6.6.r1-06700-89xx.0
    * blobs taken from bardock firmware 2.0.0
    * upstream sources repos:
     - https://source.codeaurora.org/quic/la/platform/hardware/qcom/gps
     - https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/location

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -62,7 +62,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 # Keymaster
 PRODUCT_PACKAGES += \
