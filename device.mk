eeee677d51b24a2e22834be09e4d584247a31a09
barath-k <barath.kirubakaran@gmail.com>
2018-08-04 13:38:18 +0530

    s2: Add privapp-permissions-qti.xml and qti_whitelist.xml

--- device.mk
+++ device.mk
@@ -275,6 +275,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     power.qcom
 
+# QCOM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/qti_whitelist.xml
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,${LOCAL_PATH}/rootdir,root)
