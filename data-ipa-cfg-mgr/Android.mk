b372fa6e98e8dc0beb3206f2ff210c89356dd47d
barath-k <barath.kirubakaran@gmail.com>
2018-08-05 09:22:35 +0530

    update data-ipa-cfg-mgr

--- data-ipa-cfg-mgr/Android.mk
+++ data-ipa-cfg-mgr/Android.mk
@@ -1,5 +1,6 @@
-ifeq ($(TARGET_DEVICE),s2)
+IPA_LOCAL_PATH := $(call my-dir)
 
-include $(call all-subdir-makefiles)
-
-endif
+include $(IPA_LOCAL_PATH)/hal/Android.mk
+include $(IPA_LOCAL_PATH)/ipacm/src/Android.mk
+include $(IPA_LOCAL_PATH)/ipanat/src/Android.mk
+include $(IPA_LOCAL_PATH)/ipanat/test/Android.mk
